6528f288a3b4c792b668a33d4d2bf1df5b86922b
Juraj Kubričan <kubrican.juraj@gmail.com>
2018-02-25 21:10:04 +0100

    update

--- dl.py
+++ dl.py
@@ -1,15 +1,17 @@
-import os.path
+from pathlib import Path
 
-# file = 'face.zip'
-# file = 'iris.zip'
 file = 'ear.zip'
+file = 'iris.zip'
+file = 'face.zip'
 
 # DOWNLOAD
 dl_dir = 'cache/raw/'
-if (os.path.isfile(dl_dir + file)):
+if Path(dl_dir + file).is_file():
+    print('file ' + dl_dir + file + ' already present')
+else:
+    print('downloading')
 
     import requests
-    from pathlib import Path
 
     url = 'http://147.175.106.116:81/Biometria/'
     r = requests.get(url + file, stream=True)
@@ -22,12 +24,14 @@ if (os.path.isfile(dl_dir + file)):
             if chunk:  # filter out keep-alive new chunks
                 f.write(chunk)
 
-
 # UNZIP
 extract_dir = 'cache/extract/'
-if (os.path.isfile(extract_dir + file)):
+if Path(extract_dir + file).is_file():
+    print('file ' + extract_dir + file + ' already present')
+else:
+    print('unzipping')
     import zipfile
 
     zip_ref = zipfile.ZipFile(dl_dir + file, 'r')
-    zip_ref.extractall(extract_dir)
+    zip_ref.extractall(extract_dir + file)
     zip_ref.close()
