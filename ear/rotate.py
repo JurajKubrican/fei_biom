3c560dc978b6cdb707efed68750711e9c6680390
Juraj Kubričan <kubrican.juraj@gmail.com>
2018-03-20 09:03:53 +0100

    fucking ear...

--- ear/rotate.py
+++ ear/rotate.py
@@ -16,35 +16,40 @@ Path(outDir).mkdir(parents=True, exist_ok=True)
 
 def find_contours(gray):
     gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
-    canny = cv2.Canny(gray, 50, 80, apertureSize=3, L2gradient=False)
-    print(canny.shape)
-    ret, canny = cv2.threshold(gray, 150, 255, 0)
-    print(canny.shape)
+    gray = cv2.Canny(gray, 50, 80, apertureSize=3, L2gradient=False)
+    # print(gray.shape)
+    # # ret, gray = cv2.threshold(gray, 160, 250, 0)
+    # print(gray.shape)
 
-    img, cnt, hierarchy = cv2.findContours(image=canny,
+    img, cnt, hierarchy = cv2.findContours(image=gray,
                                            mode=cv2.RETR_TREE,
                                            method=cv2.CHAIN_APPROX_NONE)
 
     cnt = np.array(cnt[0])
     hierarchy = np.array(hierarchy)
-    print(canny.shape)
-    print(img.shape)
-    print(cnt.shape)
-    print(cnt)
+    # print(gray.shape)
+    # print(img.shape)
+    # print(cnt.shape)
+    # print(cnt)
     # print(hierarchy)
     # .shape)
     # cv2.imshow('canny', img)
     # cv2.waitKey()
     gray = cv2.drawContours(gray, cnt, contourIdx=-1, color=(255, 0, 0))
-    cv2.imshow('sss', img)
-    cv2.waitKey()
+    cv2.imshow('gray', gray)
+    cv2.imshow('img', img)
 
     ellipse = cv2.fitEllipse(cnt)
+    print(ellipse)
+    ellipse_img = cv2.ellipse(img,
+                              center=ellipse[0],
+                              axes=ellipse[1],
+                              angle=ellipse[2],
+                              color=(0, 255, 0))
     # Draw both contours onto the separate images
 
     # cnt = contours[0]
     # cnt = np.array(cnt, dtype=np.uint32)
-    ellipse_img = cv2.ellipse(img, ellipse, color=(0, 255, 0))
 
     cv2.imshow('sss', ellipse_img)
     cv2.waitKey()
